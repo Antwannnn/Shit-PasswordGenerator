cb9a1bdfc6c0d5555cba816b77db7f75b5f40695
Atsura <94127380+Antwannnn@users.noreply.github.com>
2022-04-29 22:56:56 +0200

    added special characters
    at least have one special character in the password

--- LearningCourse/Program.cs
+++ LearningCourse/Program.cs
@@ -1,5 +1,5 @@
-﻿
-using System;
+﻿using System;
+using System.Text.RegularExpressions;
 
 namespace LearningCourse
 {
@@ -10,6 +10,7 @@ namespace LearningCourse
         {
             string amountOfChars;
             bool isUpperCase = true;
+            bool containsSpecialCharacters = true;
             string upperCaseValidation;
 
             Console.WriteLine("Voulez vous que votre mot de passe soit généré en majuscule (Y pour oui et N pour non) ? : ");
@@ -18,7 +19,7 @@ namespace LearningCourse
             while (true)    
             {    
                 if (upperCaseValidation.ToLower().Equals("y")) { 
-                    isUpperCase = true; 
+                    isUpperCase = true;
                     break; 
                 }
                 else if (upperCaseValidation.ToLower().Equals("n")) { 
@@ -46,37 +47,57 @@ namespace LearningCourse
 
             byte checkedNumber = checkIsNumeric(amountOfChars);
 
-            Console.WriteLine(generatePassword(checkedNumber, isUpperCase));
+            Console.WriteLine("Votre mot de passe est {0}", generatePassword(checkedNumber, isUpperCase, containsSpecialCharacters));
 
         }
-        static string generatePassword(byte size, bool isUpperCase)
-        {
-            char[] lettersAndDigits = { 'A', 'B', 'C', 'D', 'E', 'F', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-            '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+        static string generatePassword(byte size, bool isUpperCase, bool containsSpecialChar)
 
-            char[] code = new char[size];
+
+        {
+            char[] characters = { 'A', 'B', 'C', 'D', 'E', 'F', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+            '1', '2', '3', '4', '5', '6', '7', '8', '9', '{', '}', '€', '_', '-', '?'};
+
+            char[] password = new char[size];
 
             Random range = new Random();
 
-            int charIndex;
+            char chosenChar;
 
             for (int i = 0; i < size; i++)
             {
-                charIndex = range.Next(0, 35);
-                char chosenChar = lettersAndDigits[charIndex];
+                int charIndex;
 
-                code[i] = chosenChar;
+                if(containsSpecialChar)
+                  charIndex = range.Next(0, 41);
+                else
+                  charIndex = range.Next(0, 35);       
+                
+                chosenChar = characters[charIndex];
+
+                password[i] = chosenChar;
 
             }
 
-            string codeString = new string(code);
+            byte specialCharRangeInList = (byte) range.Next(36, 41);
+
+            chosenChar = characters[specialCharRangeInList];
+
+            if (containsSpecialChar)
+            {
+                if (!Array.Exists(password, element => element == chosenChar))
+                {
+                    password[range.Next(0, size)] = chosenChar;
+                }
+            }
+
+            string passwordString = new string(password);
 
             if (isUpperCase)
-                return codeString;
+                return passwordString;
 
             else
-                return codeString.ToLower();
+                return passwordString.ToLower();
             
         }
         static byte checkIsNumeric(string value)
@@ -86,7 +107,6 @@ namespace LearningCourse
             if(isNumeric) return size;
 
             else return 0;
-
         }
 
     }
